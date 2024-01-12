b76cd8c146e1feff9b357fb76a1c988410ac9129
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2024-01-12 13:48:15 +0100

    Improvement

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ async function main() {
     for (const user of allUsers) {
         console.log(user.name)
     }
+
     console.log('\nAsking for hair length long (correct):')
     allUsers = allUsers.filter(user => user.hair === 'long')
     allUsers.forEach(user => console.log(user.name))
@@ -15,6 +16,27 @@ async function main() {
     console.log('\nAsking for gender F (incorrect):')
     allUsers = allUsers.filter(user => user.gender !== 'F')
     allUsers.forEach(user => console.log(user.name))
+
+    console.log('\nAsking for beard (correct):')
+    allUsers = allUsers.filter(user => user.beard === true)
+    allUsers.forEach(user => console.log(user.name))
+
+    //same solution but with prisma query
+    // const allChars = await prisma.character.findMany({
+    //     where: {
+    //         AND: [
+    //             {
+    //                 hair: 'long',
+    //             },
+    //             {
+    //                 gender: {
+    //                     not: 'F'
+    //                 },
+    //             },
+    //         ]
+    //     }
+    // })
+    // console.log(allChars)
 
     // console.log(allUsers)
 }
